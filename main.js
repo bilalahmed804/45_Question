2d04ebc624652da2a3f6d056c4079d2962dda642
Bilal Ahmed <ba3801534@gmail.com>
2024-08-07 21:51:12 +0500

    45_Question_typescript

---
+++ main.js
@@ -0,0 +1,6 @@
+var array = [1, 2, 3, 4, 5];
+var testarrayTrue = array.includes(3);
+var testarrayFalse = array.includes(6);
+console.log(testarrayTrue);
+console.log(testarrayFalse);
+console.log(array);
